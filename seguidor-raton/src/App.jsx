28c4539b781210eb6a9a6baaeb0fb83511eb0719
Mateo Congo <aidenperse@gmail.com>
2024-11-19 16:28:08 -0500

    Seguidor raton - Agrego la dependencia de standard al proyecto. - Se crea un apartado para la implementación del seguidor del raton.

---
+++ seguidor-raton/src/App.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react'
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  // useEffect: pointer move
+  useEffect(() => {
+    console.log('effect', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove',
+        handleMove)
+    }
+
+    // Clean Up se ejecutan cuando:
+    // Cuando el compenente se desmonta
+    // Cuando se cambian las dependencias, antes de ejecutar el efecto
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  // change body className
+  useEffect(() =>{
+    document.body.classList.toggle("no-cursor", enabled)
+    return () => {
+      document.body.classList.remove("no-cursor")
+    }
+  }, [enabled])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }}
+      />
+      <button
+        onClick={() => setEnabled(!enabled)}
+      >
+        {enabled ? 'Desactivar' : 'Activar'} Seguir puntero
+      </button>
+    </>
+  )
+}
+
+function App () {
+  return (
+    <main>
+      <FollowMouse/>
+    </main>
+  )
+}
+
+export default App
